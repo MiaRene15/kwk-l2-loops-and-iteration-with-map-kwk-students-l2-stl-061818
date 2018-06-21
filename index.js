6efed6f653f8123192ed45d24255cc02510f43fe
MiaRene15 <kids.hines@icloud.com>
2018-06-21 18:45:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,14 +10,4 @@ function lowerCaseStudentNames(names) {
   return names.map(other)
 }
 
-const firstName = ['Mary', 'Joan', 'Eva', 'Frida', 'Taylor', 'Agnes']
-
-const lastName = ['Cassatt', 'Mitchell', 'Hesse', ' Kahlo', 'Swift', 'Martin']
-
-function people(students) {
-    return `firstName: ${firstName}, lastName: ${lastName}`
-}
-
-function nameToAttributes(names) {
-  return names.map(people)
-}
+const fullNames = ['Mary ']
