2453dd324b8a58cf2cb01446855d4a300950280b
MiaRene15 <kids.hines@icloud.com>
2018-06-21 18:32:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ function lowerCaseStudentNames(names) {
   return names.map(other)
 }
 
-const students = ['Mary Cassatt', 'Joan Mitchell']
+const students = ['Mary Cassatt', 'Joan Mitchell', 'Eva ']
